6857d63442bafc81d047acbb4a3969596c6aeb81
TRIOLI LOW <trioli.low@gmail.com>
2023-03-30 03:12:46 +0300

    init

---
+++ 1_home_work/1_home_work.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+
+#include<vector>
+#include<algorithm>
+
+template<class T>
+class Sort {
+public:
+    Sort(T &arr) {
+        std::sort(arr.begin(), arr.end());
+        auto it = std::unique(arr.begin(), arr.end());
+        arr.erase(it, arr.end());
+    }
+
+};
+
+
+int main()
+{
+    std::vector<int> arr{ 1, 4, 5, 7, 4, 44, 3, 1, 1, 55, 5 , 9 };
+    for (const auto& it : arr) {
+        std::cout << it << " " ;
+    }
+    std::cout << std::endl;
+    Sort <std::vector<int>> sor(arr);
+    
+    for (const auto& it : arr) {
+        std::cout << it << " ";
+    }
+    
+}
---
+++ 2_home_work/2_home_work.cpp
@@ -0,0 +1,54 @@
+﻿
+
+#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+template<class T>
+std::vector<T> print_container(std::vector<T> arr) {
+	std::cout << "This is vector: ";
+	auto prev_end = std::prev(arr.end());
+	for ( auto it = arr.begin(); it < prev_end; ++it) {
+		std::cout << *it << ", ";
+	}
+	std::cout << *prev_end;
+	std::cout << std::endl;
+	return arr;
+}
+template<class T>
+std::list<T> print_container(std::list<T> arr) {
+	std::cout << "This is list: ";
+	auto prev_end = std::prev(arr.end());
+	for (auto it = arr.begin(); it != prev_end; std::advance(it, 1)) {
+		std::cout << *it << ", ";
+	}
+	std::cout << *prev_end;
+
+	std::cout << std::endl;
+	return arr;
+}
+template<class T>
+std::set<T> print_container(std::set<T> arr) {
+	std::cout << "This is set: ";
+	auto prev_end = std::prev(arr.end());
+	for (auto it = arr.begin(); it != prev_end; std::advance(it, 1)) {
+		std::cout << *it << ", ";
+	}
+	std::cout << *prev_end;
+	std::cout << std::endl;
+	return arr;
+}
+
+int main()
+{
+
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	print_container(test_set); // four one three two. помните почему такой порядок? :)
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	print_container(test_list); // one, two, three, four
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	print_container(test_vector); // one, two, three, four
+}
+
